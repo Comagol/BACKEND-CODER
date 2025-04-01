a454972351b7ad831f61003bf8a07a7b969f2979
Comagol <comabellafrancisco@gmail.com>
2025-03-31 21:31:42 -0300

    working with asincronic code

--- clase3/liveExample.js
+++ clase3/liveExample.js
@@ -1,10 +1,15 @@
 const callbackPromesa = (resolve) => {
     const cb = () => {
-        resolve('Promesa resuelta')
+        resolve('Promesa resuelta')//Luego se llama a esto del tiempo pasado en el timeout
     }
-    setTimeout(cb,2000)
+    setTimeout(cb,2000)//luego es llamado este setTimeout
 }
 
 const miPromesa = new Promise(callbackPromesa);
 
+//primero en ejecutarse el .then
 miPromesa.then(console.log);
+//por ultimo se ejecuta el console.log
+
+//esto va a aparecer primero en la consola, ya que estamos trabajando con codigo asincrono.
+console.log('Hola desde el hilo principal')
