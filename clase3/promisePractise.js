365fc25e8f51875e4200327f7c2046fa08a242c5
Comagol <comabellafrancisco@gmail.com>
2025-04-02 11:10:02 -0300

    Change const to let, to modify the value of the bolean. it´s random now

--- clase3/promisePractise.js
+++ clase3/promisePractise.js
@@ -1,13 +1,14 @@
 const authenticator = new Promise((resolve, reject) => {
     setTimeout(() =>{
-        const isAuthenticated = false;
+        let isAuthenticated = Math.random() < 0.5;
+        console.log(isAuthenticated);
 
         if (isAuthenticated) {
             console.log("Usuario Autenticado con exito");
         } else {
             console.log("Usuario no verificado.");
         };
-    },2000);
+    },200);
  });
 
 console.log("Autenticando usuarios...");
