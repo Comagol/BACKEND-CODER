a675a6d8756ea6fad9c9a3437471536d1d69bd2c
Comagol <comabellafrancisco@gmail.com>
2025-04-04 19:59:23 -0300

    two promises handled with async and await

--- clase3/promiseAsyncAwait1-2.js
+++ clase3/promiseAsyncAwait1-2.js
@@ -12,14 +12,30 @@ function checkStock(product) {
     });
 };
 
+function confirmarPago() {
+    return new Promise ((resolve, reject) =>{
+        const paymentAprobe = Math.random() < 0.5;
+
+        if(paymentAprobe) {
+            resolve(`Pago aprovado.`);
+        } else {
+            reject(`Pago rechazado.`);
+        }
+    });
+};
+
 async function procesarCompra(product) {
     try {
         const checkingStock = await checkStock(product);
         console.log(checkingStock);
+        const waitingPayment = await confirmarPago();
+        console.log(waitingPayment);
+
+        console.log(`Compra del ${product} realizada correctamente.`)
     } catch (error) {
-        console.log(error);
+        console.log(`Error al procesar la compra de ${product}: ${error}`);
     }
-};
+}
 
 procesarCompra("banana");
 procesarCompra("pera");
