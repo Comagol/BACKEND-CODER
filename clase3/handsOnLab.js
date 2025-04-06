61f5f2ebda21e8a90e1bb670c0a45de6981ec58c
Comagol <comabellafrancisco@gmail.com>
2025-04-06 18:53:34 -0300

    adding the rest of functions

--- clase3/handsOnLab.js
+++ clase3/handsOnLab.js
@@ -8,4 +8,41 @@ function sum(a,b) {
             resolve(a+b);
         }
     });
-}
+};
+
+function subtracion(a,b) {
+    return new Promise((resolve,reject) => {
+        if(a === 0 && b === 0){
+            reject("Operacion innecesaria.");
+        } else if (a - b < 0){
+            reject("La calculadora solo devuelve opearciones positivas.");
+        } else {
+            resolve(a-b);
+        }
+    });
+};
+
+function multi(a,b) {
+    return new Promise((resolve,reject) => {
+        if(a === 0 && b === 0){
+            reject("Operacion innecesaria.");
+        } else if (a * b < 0){
+            reject("La calculadora solo devuelve opearciones positivas.");
+        } else {
+            resolve(a*b);
+        }
+    });
+};
+
+function division(a,b) {
+    return new Promise((resolve,reject) => {
+        if(a === 0 && b === 0){
+            reject("Operacion innecesaria.");
+        } else if (a / b < 0){
+            reject("La calculadora solo devuelve opearciones positivas.");
+        } else {
+            resolve(a/b);
+        }
+    });
+};
+
