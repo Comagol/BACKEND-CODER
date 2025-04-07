e3a6701b03a0fbfb156a624440cd3fe2f1ef44ff
Comagol <comabellafrancisco@gmail.com>
2025-04-07 20:54:32 -0300

    Modifing the try cacth so its less code

--- clase3/handsOnLab.js
+++ clase3/handsOnLab.js
@@ -39,7 +39,7 @@ function division(a,b) {
         if(a === 0 && b === 0){
             reject("Operacion innecesaria.");
         } else if (a / b < 0){
-            reject("La calculadora solo devuelve operaciones positivas.");
+            reject("Fallo en la division.");
         } else {
             resolve(a/b);
         }
@@ -50,27 +50,16 @@ async function calculadora(a, b) {
     try {
         const suma = await sum(a, b);
         console.log("Resultado Suma:", suma);
-    } catch (error) {
-        console.log("Error en la suma:", error);
-    }
-
-    try {
+     
         const resta = await subtracion(a, b);
         console.log("Resultado Resta:", resta);
-    } catch (error) {
-        console.log("Error en la resta:", error);
-    }
-
-    try {
+   
         const multiplicacion = await multi(a, b);
         console.log("Resultado Multiplicación:", multiplicacion);
-    } catch (error) {
-        console.log("Error en la multiplicación:", error);
-    }
 
-    try {
         const divisionResultado = await division(a, b);
         console.log("Resultado División:", divisionResultado);
+        
     } catch (error) {
         console.log("Error en la división:", error);
     }
