429d30fde5f5dfc07408c0af1f74c7fcca2146a6
Comagol <comabellafrancisco@gmail.com>
2025-04-11 18:43:15 -0300

    adding function getproductbyid, fixing problem with bites in getproducts

--- clase4/classPractice2.js
+++ clase4/classPractice2.js
@@ -1,8 +1,20 @@
 const fs = require('fs');
+const { get } = require('http');
 const path = require('path');
 
 const pathProductos = path.join(__dirname, 'productos.json');
 
-const productos = fs.readFileSync(pathProductos, 'utf-8');
+function getProducts() {
+    const productos = fs.readFileSync(pathProductos, 'utf-8');
+    return JSON.parse(productos);
+}
+
+function getProductById(pId) {
+    const products = getProducts();
+    return products.find(producto => producto.id === pId);
+}
+
+const productos = getProducts();
 
 console.log(productos);
+console.log('Producto ID:3', getProductById(3))
