69f02198e997f92424593bff38dab158476b8561
Comagol <comabellafrancisco@gmail.com>
2025-04-11 18:54:25 -0300

    adding the function addProduct

--- clase4/classPractice2.js
+++ clase4/classPractice2.js
@@ -14,7 +14,29 @@ function getProductById(pId) {
     return products.find(producto => producto.id === pId);
 }
 
+
+function addProduct(product) {
+    const products = getProducts();
+    products.push(product);
+    fs.writeFileSync(pathProductos, JSON.stringify(products))
+}
+
 const productos = getProducts();
 
+const newProduct = {
+    id: 6,
+    title: "Computadora nueva",
+    description: "Descripcion de la computadora nueva",
+    code: "LASN568",
+    price: 6800,
+    status: true,
+    stock: 5,
+    category: "Tecnologia",
+    thumbnails: ["img/laptop1.jpg","img/laptop3.jpg"]
+}
+
 console.log(productos);
-console.log('Producto ID:3', getProductById(3))
+console.log('Producto ID:3', getProductById(3));
+
+addProduct(newProduct);
+console.log(productos);
