b7489d212609f2d1f8c186cf413d189ad2469849
Comagol <comabellafrancisco@gmail.com>
2025-04-11 18:20:27 -0300

    playing with the calculator

--- clase4/classPractice.js
+++ clase4/classPractice.js
@@ -5,16 +5,18 @@ const moment = require("moment");
 const actualDate = moment();
 
 // fecha de naciemiento
-const birthday = moment('2006-01-26', 'YYYY-MM-DD');
+const birthday = moment('2024-04-30', 'YYYY-MM-DD');
 
 // Validar si la fecha de naciemiento es valida
 if(!birthday.isValid()) {
-    console.log("❌ La fecha de cumpleaños no es valida.");
+    console.log("❌ La fecha no es valida.");
 } else {
     // Devolvemos la diferencia en dias desde su nacimiento.
     const daysSinceBirth = actualDate.diff(birthday, "days");
+    const secondsSinceBirth = actualDate.diff(birthday, "seconds");
 
     console.log("fecha actual: ", actualDate.format('YYYY-MM-DD'));
     console.log("fecha de nacimiento: ", birthday.format('YYYY-MM-DD'));
-    console.log(`Han pasado ${daysSinceBirth} días desde tu nacimiento.`)
+    console.log(`Han pasado ${daysSinceBirth} días desde tu nacimiento.`);
+    console.log(`Han pasado ${secondsSinceBirth} segundos desde tu nacimiento.`);
 }
