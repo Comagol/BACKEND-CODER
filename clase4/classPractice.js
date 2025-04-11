b2c8e97e5b8f85d39c68e3d08b9a7e08ca0ec1ce
Comagol <comabellafrancisco@gmail.com>
2025-04-11 18:12:16 -0300

    first practise with moment

--- clase4/classPractice.js
+++ clase4/classPractice.js
@@ -1,14 +1,20 @@
-const fs = require ('fs')
+//Importamos moment con require.
+const moment = require("moment");
 
-async function writeFile() {
-    const data = "Contenido que se va a escribir en el archivo.txt"
+// Creamos una variable con la fecha actual.
+const actualDate = moment();
 
-    try {
-        await fs.writeFile('archivo.txt', data)
-        console.log("Archivo creado y escrito con exito")
-    } catch (error){
-        console.error("Error al crear el archivo", error)
-    }
+// fecha de naciemiento
+const birthday = moment('2006-01-26', 'YYYY-MM-DD');
+
+// Validar si la fecha de naciemiento es valida
+if(!birthday.isValid()) {
+    console.log("❌ La fecha de cumpleaños no es valida.");
+} else {
+    // Devolvemos la diferencia en dias desde su nacimiento.
+    const daysSinceBirth = actualDate.diff(birthday, "days");
+
+    console.log("fecha actual: ", actualDate.format('YYYY-MM-DD'));
+    console.log("fecha de nacimiento: ", birthday.format('YYYY-MM-DD'));
+    console.log(`Han pasado ${daysSinceBirth} días desde tu nacimiento.`)
 }
-
-writeFile()
