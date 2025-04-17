64ee6d1848709e81eb1c79185bfcf33dbcd3a961
Comagol <comabellafrancisco@gmail.com>
2025-04-17 15:46:55 -0300

    finishing the exercise

--- clase4/handsOnLab/server.js
+++ clase4/handsOnLab/server.js
@@ -19,7 +19,7 @@ app.get('/products', (req,res) =>{
 });
 
 //Endpoint para obtener un producto por id
-app.post('products/:pid', (req,res) =>{
+app.get('/products/:pid', (req,res) =>{
     const {pid} = req.params;
     const product = productManager.getProductById(pid);
     if (!product) {
@@ -28,7 +28,28 @@ app.post('products/:pid', (req,res) =>{
     res.json(product);
 });
 
+app.post('/products/', (req,res) => {
+    const {title, description, price, thumbnail, code, stock} = req.body;
+
+    //validacion basica para que los campos esten completos
+    if(!title || !description || !price || !thumbnail || !code || !stock) {
+        return res.status(400).json({mensaje: 'Faltan campos obligatorios.'});
+    }
+
+    const nuevoProducto = {
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock
+    };
+
+    const productoAgregado = productManager.addProduct(nuevoProducto);
+    res.status(201).json({mensaje: 'producto agregado', producto: productoAgregado });
+});
+
 //inicio del servidor
 app.listen(PORT, () =>{
     console.log(`Servidor levantado y corriendo en el puerto${PORT}`)
-})
+});
