35ba714936431b083d10070a8ba58120a4d55e1d
Comagol <comabellafrancisco@gmail.com>
2025-04-25 17:05:09 -0300

    ADDING A FORM TO REGISTER USERS

--- clase5/5.2/routers/users.router.js
+++ clase5/5.2/routers/users.router.js
@@ -1,23 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-let users = [
-    { nombre: "Juan", apellido: "Pérez", edad: 30, correo: "juan@mail.com", telefono: "123456789" },
-    { nombre: "Ana", apellido: "Gómez", edad: 25, correo: "ana@mail.com", telefono: "987654321" },
-    { nombre: "Luis", apellido: "Martínez", edad: 40, correo: "luis@mail.com", telefono: "456789123" },
-    { nombre: "Lucía", apellido: "Rodríguez", edad: 28, correo: "lucia@mail.com", telefono: "321654987" },
-    { nombre: "Carlos", apellido: "López", edad: 35, correo: "carlos@mail.com", telefono: "741852963" },
-];
-
+let users = [];
+/*
 router.get('/', (req,res) => {
     const randomUser = users[Math.floor(Math.random() * users.length)];
     res.render('home', randomUser);
+});*/
+
+router.get('/register', (req,res) => {
+    res.render('register');
 });
 
-router.post('/', (req,res) => {
-    const user = req.body;
-    users.push(user);
-    res.status(201).json({ mensaje: 'Usuario agregado', user});
-});
+router.post('/user', (req, res) => {
+    const { name, email, password } = req.body;
+  
+    if (!name || !email || !password) {
+      return res.status(400).send('Todos los campos son obligatorios');
+    }
+  
+    const newUser = { name, email, password };
+    users.push(newUser);
+  
+    res.status(200).send(`Usuario ${name} registrado correctamente`);
+  });
+
+  router.get('/', (req, res) => {
+    res.status(200).json({ users });
+  });
 
 module.exports = router;
