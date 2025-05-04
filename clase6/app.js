e935f0db1505608f956cae8a77a63f4595c7caa7
Comagol <comabellafrancisco@gmail.com>
2025-05-04 16:12:18 -0300

    finishing the configuration of socket.io

--- clase6/app.js
+++ clase6/app.js
@@ -5,10 +5,25 @@ const io = require(socket.io)(http)
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, '/public')));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
+app.use(express.static(path.join(__dirname, '/public')));
+
+let messages = [];
+
+//configuracion Socket.io
+io.on('connection', (socket) => {
+    socket.emit("messageList", messages);
+    console.log("Nuevo cliente se ha conectado");
+
+    socket.on('newMessage', (message) => {
+        messages.push(message);
+        io.emit('newMessage', {
+            socketId : socket.id, message: message
+        })
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server runing in port ${PORT}`);
