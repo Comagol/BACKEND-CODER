e4ac3251190163e1c5f907812dd02252574ce4eb
Comagol <comabellafrancisco@gmail.com>
2025-05-07 23:35:51 -0300

    fixing the code

--- clase7/7.1/app.js
+++ clase7/7.1/app.js
@@ -3,6 +3,21 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
+async function buscarUsuarioPorNombre(nombre, usersCollection) {
+  try {
+    const usuario = await usersCollection.findOne({ nombre: nombre });
+
+    if (usuario) {
+      console.log("Usuario encontrado:");
+      console.log(usuario);
+    } else {
+      console.log("No se encontró ningún usuario con ese nombre.");
+    }
+  } catch (error) {
+    console.error("Error al buscar el usuario:", error);
+  }
+}
+
 async function main() {
   try {
     await client.connect();
@@ -22,26 +37,15 @@ async function main() {
     const users = await usersCollection.find().toArray();
     console.log("Usuarios en la base:");
     console.log(users);
+
+    // Buscar usuario por nombre
+    await buscarUsuarioPorNombre("Francisco", usersCollection);
+
   } catch (error) {
     console.error("Error:", error);
   } finally {
     await client.close();
   }
 }
-
-async function buscarUsuarioPorNombre(nombre) {
-    try {
-      const usuario = await usersCollection.findOne({ nombre: nombre });
-  
-      if (usuario) {
-        console.log("Usuario encontrado:");
-        console.log(usuario);
-      } else {
-        console.log("No se encontró ningún usuario con ese nombre.");
-      }
-    } catch (error) {
-      console.error("Error al buscar el usuario:", error);
-    }
-  }
 
 main();
